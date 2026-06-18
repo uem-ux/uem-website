eae5422434038078f2aed6ddcee8856d393b8727
uem-ux <a.zakaria23@gmail.com>
2026-06-18 15:55:50 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
-})
+export default defineConfig({ plugins: [react()] })
